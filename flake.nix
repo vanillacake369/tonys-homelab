ae5da508432eb289c993c80ca5dc85414959d9e8
limjihoon <lonelynight1026@gmail.com>
2026-01-21 22:25:13 +0900

    refactor(colmena): split host/vm nodes and tagging
    Separate host/VM hive entries and introduce tag-based grouping.

--- flake.nix
+++ flake.nix
@@ -28,37 +28,30 @@
   outputs = {nixpkgs, ...} @ inputs: let
     inherit (nixpkgs) lib;
 
-    # 1. 아키텍처 및 시스템 설정
+    # 아키텍처 및 시스템 설정
     mainSystem = "x86_64-linux";
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = f: lib.genAttrs supportedSystems f;
 
-    # 2. CI 환경 감지 및 상수 로드
-    isCI = builtins.getEnv "CI" == "true";
-    homelabConstants = let
-      realFile = ./lib/homelab-constants.nix;
-      exampleFile = ./lib/homelab-constants-example.nix;
-    in
-      if builtins.pathExists realFile
-      then import realFile
-      else import exampleFile;
-    # homelabConstants = import (
-    #   if isCI || !(builtins.pathExists ./lib/homelab-constants.nix)
-    #   then ./lib/homelab-constants-example.nix
-    #   else ./lib/homelab-constants.nix
-    # );
-
-    # 3. 공통 아규먼트 (SpecialArgs)
+    # 공통 인자 및 상수
+    # NOTE : sshPublicKey 는 외부에서 주입되는 값
+    # flake 외부 환경변수는 --impure 실행 시에만 유효함
+    homelabConstants = import ./lib/homelab-constants.nix;
     specialArgs = {
       inherit inputs homelabConstants;
       homelabConfig = homelabConstants.host;
-      # Flake 외부 환경변수는 --impure 실행 시에만 유효함
-      sshPublicKey = builtins.getEnv "SSH_PUB_KEY";
+      sshPublicKey = let
+        envKey = builtins.getEnv "SSH_PUB_KEY";
+      in
+        if envKey != ""
+        then envKey
+        else "";
+      isCI = false;
     };
 
-    # 4. MicroVM 생성기 (DRY - Don't Repeat Yourself)
+    # MicroVM 생성기 (DRY - Don't Repeat Yourself)
     mkMicroVMs = hostConfig: let
-      vms = ["vault" "jenkins" "registry" "k8s-master" "k8s-worker-1" "k8s-worker-2"];
+      vms = ["opnsense"];
       vmSpecialArgs =
         specialArgs
         // {
@@ -71,14 +64,14 @@
         autostart = true;
       });
 
-    # 5. 공유 모듈 (NixOS & Colmena 공통)
-    sharedModules = [
+    # 호스트 공통 모듈 (NixOS & Colmena 공통)
+    hostModules = [
       inputs.disko.nixosModules.disko
       inputs.sops-nix.nixosModules.sops
       inputs.microvm.nixosModules.host
       inputs.home-manager.nixosModules.home-manager
       ./configuration.nix
-      ({config, ...}: {
+      ({config, isCI, ...}: {
         home-manager = {
           useGlobalPkgs = true;
           useUserPackages = true;
@@ -95,12 +88,19 @@
         };
 
         # CI 환경에서는 빌드 부하를 줄이기 위해 MicroVM 정의를 비움
+        # 호스트는 부트스트랩용 최소 VM만 관리
         microvm.host.enable = true;
         microvm.vms =
           if isCI
           then {}
           else mkMicroVMs config;
       })
+    ];
+
+    # VM 공통 모듈 (Colmena 노드용)
+    # sops는 VM의 SSH host key를 복호화 키로 사용
+    vmModules = [
+      ./modules/nixos/sops.nix
     ];
   in {
     # 모든 지원 아키텍처에서 colmena 패키지 사용 가능
@@ -112,11 +112,22 @@
     nixosConfigurations.homelab = lib.nixosSystem {
       system = mainSystem;
       inherit specialArgs;
-      modules = sharedModules;
+      modules = hostModules;
+    };
+
+    # CI용 (MicroVM 제외)
+    nixosConfigurations.homelabCi = lib.nixosSystem {
+      system = mainSystem;
+      specialArgs = specialArgs // {isCI = true;};
+      modules = hostModules;
     };
 
     # 원격 배포용 (colmena)
-    colmenaHive = inputs.colmena.lib.makeHive {
+    # TODO : 홈랩용 Colmena, 각 VM 용 Colmena 를 분리해야함
+    # TODO : ./justfile 의 deploy 는 전체 배포로 남겨두되
+    # 분리한 Colmena 별로 deploy 를 따로 구성하여
+    # fractional changes 에 대해서 배포가능하게끔 수정해야함
+    colmenaHive = inputs.colmena.lib.makeHive ({
       meta = {
         nixpkgs = import nixpkgs {system = mainSystem;};
         inherit specialArgs;
@@ -125,11 +136,33 @@
         deployment = with homelabConstants.host.deployment; {
           inherit targetHost targetUser;
           buildOnTarget = true;
-          tags = ["physical" "homelab"];
+          tags = ["physical" "homelab" "host"];
         };
-        imports = sharedModules;
+        imports = hostModules;
       };
-    };
+    }
+      // lib.mapAttrs (name: vmInfo: {
+        deployment = {
+          targetHost = vmInfo.ip;
+          targetUser = vmInfo.deployment.user;
+          tags = vmInfo.deployment.tags;
+        };
+        imports = vmModules ++ [
+          ./vms/${name}.nix
+          ({...}: {
+            users.users.${vmInfo.deployment.user}.openssh.authorizedKeys.keys =
+              lib.optional (specialArgs.sshPublicKey != "") specialArgs.sshPublicKey;
+
+            services.openssh.hostKeys = [
+              {
+                path = "/etc/ssh/ssh_host_ed25519_key";
+                type = "ed25519";
+              }
+            ];
+          })
+        ];
+      })
+      homelabConstants.vms);
 
     # 외부에서 상수를 참조할 수 있도록 노출
     inherit homelabConstants;

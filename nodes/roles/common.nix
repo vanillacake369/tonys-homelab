d13955045cc5ae6784dd4ecfbaa8aef4e3c12bc2
limjihoon <lonelynight1026@gmail.com>
2026-04-19 03:02:33 +0900

    feat: implement role-based node templates
    - common.nix: base atoms for all nodes (nix, locale, shell, ssh, firewall, keys)
    - host.nix: physical host additions (common + sops + tailscale)
    - k8s-master.nix: master additions (common + k8s atoms + control plane ports)
    - k8s-worker.nix: worker additions (common + k8s atoms + kubelet port)

---
+++ nodes/roles/common.nix
@@ -0,0 +1,13 @@
+# nodes/roles/common.nix
+# 모든 homelab 노드가 가져야 할 공통 Atom 전략
+{ ... }: {
+  imports = [
+    ../../atoms/system/nix.nix
+    ../../atoms/system/locale.nix
+    ../../atoms/system/shell.nix
+    ../../atoms/system/cli-tools.nix
+    ../../atoms/network/ssh-server.nix
+    ../../atoms/network/network-base.nix
+    ../../atoms/system/authorized-keys.nix
+  ];
+}
---
+++ nodes/roles/k8s-worker.nix
@@ -0,0 +1,28 @@
+# nodes/roles/worker.nix
+# K8s Worker Node Strategy
+{ pkgs, ... }: {
+  imports = [
+    ./common.nix
+    ../../atoms/k8s/container-runtime.nix
+    ../../atoms/k8s/kubelet-service.nix
+    ../../atoms/k8s/cni-cilium.nix
+    ../../atoms/k8s/k8s-distro-compat.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    kubernetes
+    cri-tools
+    etcd
+    kubernetes-helm
+  ];
+
+  # K8s worker 포트 (network-base의 [22]에 병합됨)
+  networking.firewall.allowedTCPPorts = [10250];
+
+  # OS Mocking for kubeadm
+  systemd.tmpfiles.rules = [
+    "L+ /usr/bin/socat - - - - ${pkgs.socat}/bin/socat"
+    "L+ /usr/bin/mount - - - - ${pkgs.util-linux}/bin/mount"
+    "L+ /var/run/containerd/containerd.sock - - - - /run/containerd/containerd.sock"
+  ];
+}
